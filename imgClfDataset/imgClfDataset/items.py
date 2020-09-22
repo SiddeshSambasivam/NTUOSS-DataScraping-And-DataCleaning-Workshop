22bf9629d6e01332622f5ec28fa61d3fcdee540d
siddesh <plutocrat45@gmail.com>
2020-09-22 11:52:25 +0800

    compeleted task 1

--- imgClfDataset/imgClfDataset/items.py
+++ imgClfDataset/imgClfDataset/items.py
@@ -7,6 +7,8 @@ import scrapy
 
 
 class ImgclfdatasetItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+
+    image_titles = scrapy.Field()
+    images = scrapy.Field()
+    image_urls = scrapy.Field()
+    
--- imgClfDataset/imgClfDataset/spiders/images.py
+++ imgClfDataset/imgClfDataset/spiders/images.py
@@ -1,24 +1,34 @@
 import scrapy
-from RawData.items import  Rawdataitem
 from scrapy.loader import ItemLoader
+from ..items import ImgclfdatasetItem
 
 class images(scrapy.Spider):
-
-    name = "raw_paint"
+    
+    name = 'images'
+    classname = "photographs" # [ "photographs", "paintings"]
     start_urls = [
-    "https://fineartamerica.com/art/photographs"
+        f"https://fineartamerica.com/art/{classname}"
     ]
     page = 1
+    image_urls = []
+    image_titles = []
+    page_limit = 10
 
     def parse(self, response):
-        title = response.xpath('//div[@class= "searchengineresultdiv"]/div/a/img/@title').extract()
-        urls = response.xpath('//div[@class= "searchengineresultdiv"]/div/a/img/@data-src').extract()      
+        
+        self.image_titles = response.xpath('//p[@class="flowArtworkName"]//text()').extract()
+        self.image_urls  = response.xpath('//div[@class="flowImageContainerDiv"]/a/img/@data-src').extract()
+        
         self.page= self.page + 1
-        for url, name in zip(urls, title):
-            yield Rawdataitem(file_urls = [url], title= name)
-
-        next_page = f'https://fineartamerica.com/art/photographs?page={self.page}'
-        yield scrapy.Request(next_page, self.parse)
+        
+        if self.page <= self.page_limit:        
+            item = ImgclfdatasetItem()
+            item['image_urls'] = self.image_urls
+            item['image_titles'] = self.image_titles
+            yield item                
+            
+            next_page = f'https://fineartamerica.com/art/{self.classname}?page={self.page}'
+            yield scrapy.Request(next_page, callback=self.parse)
         
 
                 
